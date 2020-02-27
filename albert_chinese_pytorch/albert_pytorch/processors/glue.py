bc7f49e60befcf3264289c6ffcf5e6ebc8a89af5
Terry chan <napoler2008@gmail.com>
2020-02-28 02:08:33 +0800

    Signed-off-by: Terry chan <napoler2008@gmail.com>

--- albert_chinese_pytorch/albert_pytorch/processors/glue.py
+++ albert_chinese_pytorch/albert_pytorch/processors/glue.py
@@ -96,6 +96,7 @@ def glue_convert_examples_to_features(examples, tokenizer,
         
         #自动屏蔽15%的文字信息
         maskid=tokenizer.convert_tokens_to_ids("[MASK]")
+        print(inputs["input_ids"])
         if len(inputs["input_ids"])>3:
             for num in sample(range(1,len(inputs["input_ids"])),0.15*len(inputs["input_ids"])):
                 inputs["input_ids"][num] =maskid[0]
